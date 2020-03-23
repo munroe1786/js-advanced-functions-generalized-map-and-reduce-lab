6f6716fb0282e331985d2b281ba391cf7b72b80e
munroe1786 <era3munroe@gmail.com>
2020-03-23 06:30:14 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your functions here
+function map(src, cb) {
+    let a = []
+    for (let i = 0; i < src.length; i++) {
+        let theElement = src[i]
+        a.push(cb(theElement))
+    }
+    return a
+}
+
+function reduce(src, cb, starting) {
+    let r = (!!starting) ? starting : src [0]
+    let i = (!!starting) ? 0 : 1
+
+    for (; i < src.length; i++) {
+        r = cb(src[i], r)
+    }
+    return r
+}
